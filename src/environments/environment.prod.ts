7912484521c4b30196d07238c5c3ab2c059c58b0
cigotete <cigotete@gmail.com>
2017-08-06 19:35:07 -0500

    Added prod/test environment config cons values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyD-H_x2hwvLbL_qjo__V8iHpgbKWqNdEdw",
+    authDomain: "uploading-images-c08cd.firebaseapp.com",
+    databaseURL: "https://uploading-images-c08cd.firebaseio.com",
+    projectId: "uploading-images-c08cd",
+    storageBucket: "uploading-images-c08cd.appspot.com",
+    messagingSenderId: "982598631430"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyD-H_x2hwvLbL_qjo__V8iHpgbKWqNdEdw",
+    authDomain: "uploading-images-c08cd.firebaseapp.com",
+    databaseURL: "https://uploading-images-c08cd.firebaseio.com",
+    projectId: "uploading-images-c08cd",
+    storageBucket: "uploading-images-c08cd.appspot.com",
+    messagingSenderId: "982598631430"
+  }
 };
